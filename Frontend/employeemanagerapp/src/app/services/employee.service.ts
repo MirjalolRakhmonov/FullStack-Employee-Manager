fb4e1dbbcd162a8caa1bb605d20103ce0379590c
Mirjalol Rakhmonov <manhattan701@gmail.com>
2021-03-20 00:51:53 +0100

    Connecting to Backend server and CORS Configuration

--- Frontend/employeemanagerapp/src/app/services/employee.service.ts
+++ Frontend/employeemanagerapp/src/app/services/employee.service.ts
@@ -2,31 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from '../employee';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class EmployeeService {
+  private apiServerUrl = environment.apiBaseUrl;
 
-  private apiServerUrl = '';
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient){}
 
   public getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/api/employee/all`);
   }
 
-  public addEmployees(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/api/employee/add`, employee);
   }
 
-  public updateEmployees(employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/api/employee/update`, employee);
   }
 
-  public deleteEmployees(employeeId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/api/employee/delete/${employeeId}`);
   }
-
 }
-
